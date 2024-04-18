35b3878e2b2b9991751c109ce06d1be398e21d23
Takaaki Masaki <takaakimasaki@users.noreply.github.com>
2024-04-18 09:02:49 +0200

    Update 2-lecture_sample_code.R

--- code/day2/2-lecture_sample_code.R
+++ code/day2/2-lecture_sample_code.R
@@ -15,23 +15,34 @@ pacman::p_load(here,sf,terra,tidyverse,dplyr,tmap,exactextractr,paletteer)
 admin1_sf <- st_read("data-raw/tza_admbnda_adm1_20181019/tza_admbnda_adm1_20181019.shp") %>%
   st_make_valid() #we will cover this function more closely on Day 2
 
-tmap_mode("plot")
-tm_shape(admin1_sf, projection = 8857) +
-  tm_borders()
+# let's reproject from EPSG4326 to UTM (EPSG 21037 for Tanzania according to https://epsg.io/)
+admin1_sf_rpj <- admin1_sf %>%
+  st_transform(21037) #this function transforms the given object
 
-tm_shape(admin1_sf) +
-  tm_borders(col="blue")
+tmap_mode("plot")
+tm_shape(admin1_sf_rpj) +
+  tm_borders() 
+
+# now reproject based on different EPSG code
+admin1_sf_rpj_v2 <- admin1_sf %>%
+  st_transform(2736) #this code is typically used for Mozambique
+
+# compare two objects reprojected to different CRS
+e <- c(-548109.3,1317237,9000000,10000000)
+plot(st_geometry(admin1_sf_rpj), border = "red", xlim = c(e[1], e[3]), ylim = c(e[2], e[4]))
+plot(st_geometry(admin1_sf_rpj_v2), border = "blue", xlim = c(e[1], e[3]), ylim = c(e[2], e[4]), add=TRUE)
 
 # read raster data
 pop<-terra::rast("data-raw/tza_ppp_2020_UNadj_constrained.tif")
+
 # for better visualization, you may aggregate raster object
 pop_10 <-terra::aggregate(pop, fact=10, fun="sum", na.rm=TRUE)
 plot(pop_10)
 
-# tm_shape(admin1_sf, projection = 8857) +
-tmap_mode("view")
-tm_shape(pop_10, raster.warp = FALSE) + 
-  tm_raster(palette = "viridis", style="quantile", n=10) + 
-  tm_shape(admin1_sf, projection = 8857) +
-  tm_borders() 
-  
+# you can also reproject raster data
+pop_10_rpj <- project(pop_10, crs(admin1_sf_rpj_v2)) #projected to EPSG code more appropriate for Mozambique, not Tanzania!
+
+# let's see how these different datasets overlap with each other.
+plot(pop_10_rpj)
+plot(st_geometry(admin1_sf_rpj), add=TRUE)
+plot(st_geometry(admin1_sf_rpj_v2), border="blue", add=TRUE)
